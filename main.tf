5b47d8f7093a36eb4a80ed29a3cacda447d90a95
sanjay <sanjayrohilla@gmail.com>
2022-01-16 20:15:56 +1100

    destroy stage added

--- main.tf
+++ main.tf
@@ -9,16 +9,16 @@ terraform {
 
 provider "docker" {}
 #Pull a container image
- resource "docker_image" "nodered_image" {
-   name = "nodered/node-red"
- }
+resource "docker_image" "nodered_image" {
+  name = "nodered/node-red"
+}
 
 # Run a container
- resource "docker_container" "nodered_container" {
-   name = "nodered-container"
-   image = docker_image.nodered_image.latest
-   ports {
-     internal = 1880
-     external = 1880
-   }
- }
+resource "docker_container" "nodered_container" {
+  name  = "nodered-container"
+  image = docker_image.nodered_image.latest
+  ports {
+    internal = 1880
+    external = 1880
+  }
+}
